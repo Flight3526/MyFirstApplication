23dbf6f7da20cc41aa5e6e6823022f8bfda5e7e2
Flight3526 <106138521+Flight3526@users.noreply.github.com>
2023-10-08 16:57:51 +0800

    添加RecyclerView，启动activity时显示，优化界面

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,24 +1,25 @@
 package com.jnu.student;
-
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView text_view_greet;
     private TextView text_view_hello;
     private TextView text_view_jnu;
+    ActivityResultLauncher<Intent> launcher;
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView text_view_hello_world;
 //        //不使用layout文件，纯Java代码显示Hello World
 //        TextView text_view_hello = new TextView(this);
 //        text_view_hello.setText(R.string.hello_world);
@@ -27,35 +28,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //使用layout文件显示布局
         setContentView(R.layout.activity_main);
 
-        text_view_hello_world = findViewById(R.id.text_view_hello_world);  //根据ID获取响应对象
-        text_view_hello_world.setText(R.string.hello_android);       //修改原先内容
-        text_view_hello_world.setTextColor(Color.RED);               //修改字体颜色
-        text_view_hello_world.setTextSize(25);                       //修改字体大小
-
+        text_view_greet = findViewById(R.id.text_view_hello_world);  // 根据ID获取响应对象
+        text_view_greet.setText(R.string.hello_android);             // 修改原先内容
+        text_view_greet.setTextColor(Color.RED);                     // 修改字体颜色
+        text_view_greet.setTextSize(30);                             // 修改字体大小
         text_view_hello = findViewById(R.id.text_view_hello);
-        text_view_hello.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);   //设置文字居中
+        text_view_hello.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);      // 设置文字居中
         text_view_hello.setTextColor(Color.BLUE);
-        text_view_hello.setTextSize(25);
+        text_view_hello.setTextSize(30);
         text_view_jnu = findViewById(R.id.text_view_jnu);
         text_view_jnu.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         text_view_jnu.setTextColor(Color.GREEN);
-        text_view_jnu.setTextSize(25);
+        text_view_jnu.setTextSize(30);
 
         Button button_change_text = findViewById(R.id.button_change_text);
         button_change_text.setOnClickListener(this);
 
         Button button_change_color = findViewById(R.id.button_change_color);
-        button_change_color.setOnClickListener(view -> {
+        button_change_color.setOnClickListener(view -> {       // 设置监听器, 使三个TextView交换颜色
             Button button_change = (Button)view;
-            button_change.setBackgroundColor(Color.YELLOW);
-            int tempColor = text_view_hello.getCurrentTextColor();
-            text_view_hello.setTextColor(text_view_jnu.getCurrentTextColor());
-            text_view_jnu.setTextColor(tempColor);
-            button_change.setBackgroundColor(text_view_jnu.getCurrentTextColor());
+            button_change.setBackgroundColor(text_view_hello.getCurrentTextColor());
+            int tempColor1 = text_view_greet.getCurrentTextColor();
+            int tempColor2 = text_view_jnu.getCurrentTextColor();
+            text_view_greet.setTextColor(text_view_hello.getCurrentTextColor());
+            text_view_jnu.setTextColor(tempColor1); text_view_hello.setTextColor(tempColor2);
+        });
+
+        launcher = registerForActivityResult(                     // 创建ActivityResultLauncher启动器对象
+            new ActivityResultContracts.StartActivityForResult(), // 使用StartActivityForResult合同
+            result -> {}
+        );
+        Button button_show_recycler = findViewById(R.id.button_show_recyclerview);
+        button_show_recycler.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RecyclerviewBook.class);
+            launcher.launch(intent);                              //启动Activity
         });
     }
 
-    public void onClick(View view){                         //交换TextView文本并显示Toast和对话框
+    public void onClick(View view){                         // 交换TextView文本并显示Toast和对话框
         CharSequence tempText = text_view_hello.getText();
         text_view_hello.setText(text_view_jnu.getText());
         text_view_jnu.setText(tempText);
@@ -65,6 +75,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .setPositiveButton("确定", (dialog, id) -> {})
                 .setNegativeButton("取消", (dialog, id) -> {});
         AlertDialog dialog = builder.create();
-        dialog.show();                                      //显示对话框
+        dialog.show();                                      // 显示对话框
     }
 }
---
+++ app/src/main/java/com/jnu/student/RecyclerviewBook.java
@@ -0,0 +1,32 @@
+package com.jnu.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerviewBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_book);
+        Log.i("RecyclerviewBook","enter OnCreate");
+
+        RecyclerView recycler_View_books = findViewById(R.id.book_view);
+        recycler_View_books.setLayoutManager(new LinearLayoutManager(this));
+        List<BookItem> booklist = new ArrayList<>();
+        for(int i=0;i<20;i++) {
+            booklist.add(new BookItem("软件项目管理案例教程（第4版）", "30元", R.drawable.book_2));
+            booklist.add(new BookItem("创新工程实践", "25元", R.drawable.book_no_name));
+            booklist.add(new BookItem("信息安全教学基础（第2版）", "35元", R.drawable.book_1));
+        }
+        MyAdapter bookAdapter = new MyAdapter(booklist);
+        recycler_View_books.setAdapter(bookAdapter);
+    }
+}
